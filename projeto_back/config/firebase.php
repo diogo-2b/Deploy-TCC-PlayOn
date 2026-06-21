fc2a5245db3c538098c64c9c87159eda22742f13
Diogo-b-b <diogobbrum2005@gmail.com>
2026-06-21 17:47:15 -0300

    fix: Ajuste de Firebase para deploy

--- projeto_back/config/firebase.php
+++ projeto_back/config/firebase.php
@@ -1,11 +1,18 @@
 <?php
+// Código antes deploy funcionando
+// $credentials = env('FIREBASE_CREDENTIALS', 'storage/app/firebase/firebase.json');
+// if ($credentials && !str_starts_with($credentials, '/') && !preg_match('/^[A-Za-z]:\\\\/', $credentials)) {
+//     $credentials = base_path($credentials);
+// }
+// return [
+//     'credentials' => $credentials,
+// ];
+?>
 
-$credentials = env('FIREBASE_CREDENTIALS', 'storage/app/firebase/firebase.json');
-
-if ($credentials && !str_starts_with($credentials, '/') && !preg_match('/^[A-Za-z]:\\\\/', $credentials)) {
-    $credentials = base_path($credentials);
-}
-
+<?php
+$json = env('FIREBASE_CREDENTIALS_JSON');
 return [
-    'credentials' => $credentials,
+    'credentials' => $json
+        ? json_decode($json, true)
+        : base_path(env('FIREBASE_CREDENTIALS', 'storage/app/firebase/firebase.json')),
 ];
